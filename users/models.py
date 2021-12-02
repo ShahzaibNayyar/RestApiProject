8ad2062be264f0562849beab2327891169999425
ShahzaibNayyar <shahzaib.nayyar@gmail.com>
2021-12-02 12:18:12 +0500

    Modified Django authentication schema from username to email

--- users/models.py
+++ users/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def _create_user(self,email, password, first_name, last_name,phone_number,age,gender, **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        if not password:
+            raise ValueError('Password is required')
+        user = self.model(
+            email = self.normalize_email(email),
+            first_name = first_name,
+            last_name = last_name,
+            phone_number = phone_number,
+            age = age,
+            gender = gender,
+            **extra_fields,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self,email, password, first_name, last_name,phone_number,age,gender, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email=email,password=password,first_name=first_name,last_name=last_name,phone_number=phone_number,age=age,gender=gender,**extra_fields)
+
+    def create_superuser(self,email, password, first_name, last_name,phone_number,age,gender, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email=email,password=password,first_name=first_name,last_name=last_name,phone_number=phone_number,age=age,gender=gender,**extra_fields)
+
+class User(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(db_index=True, unique=True, max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=30)
+
+    is_staff = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name','last_name','phone_number', 'age','gender']
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+
