e9a3318e5e2b798f2792f9a11e1a42bc3f7f918a
ShahzaibNayyar <shahzaib.nayyar@gmail.com>
2021-12-02 13:15:18 +0500

    Added UserProfile and RestFramework

--- users/models.py
+++ users/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-
 class CustomUserManager(BaseUserManager):
-    def _create_user(self,email, password, first_name, last_name,phone_number,age,gender, **extra_fields):
+    def _create_user(self,email, password, first_name, last_name, **extra_fields):
         if not email:
             raise ValueError('Email is required')
         if not password:
@@ -11,34 +10,28 @@ class CustomUserManager(BaseUserManager):
             email = self.normalize_email(email),
             first_name = first_name,
             last_name = last_name,
-            phone_number = phone_number,
-            age = age,
-            gender = gender,
             **extra_fields,
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self,email, password, first_name, last_name,phone_number,age,gender, **extra_fields):
+    def create_user(self,email, password, first_name, last_name, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email=email,password=password,first_name=first_name,last_name=last_name,phone_number=phone_number,age=age,gender=gender,**extra_fields)
+        return self._create_user(email=email,password=password,first_name=first_name,last_name=last_name,**extra_fields)
 
-    def create_superuser(self,email, password, first_name, last_name,phone_number,age,gender, **extra_fields):
+    def create_superuser(self,email, password, first_name, last_name, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_superuser', True)
-        return self._create_user(email=email,password=password,first_name=first_name,last_name=last_name,phone_number=phone_number,age=age,gender=gender,**extra_fields)
+        return self._create_user(email=email,password=password,first_name=first_name,last_name=last_name,**extra_fields)
 
 class User(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(db_index=True, unique=True, max_length=255)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=20)
-    age = models.IntegerField()
-    gender = models.CharField(max_length=30)
 
     is_staff = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
@@ -47,10 +40,16 @@ class User(AbstractBaseUser,PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name','last_name','phone_number', 'age','gender']
+    REQUIRED_FIELDS = ['first_name','last_name']
 
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=30)
 
 
